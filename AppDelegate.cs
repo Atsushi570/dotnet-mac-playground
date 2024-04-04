9b6a43e465b88b7f8621cceac9e72992ddf351ac
Atsushi Sato <sato.atsushi570@gmail.com>
2024-04-04 17:50:10 +0900

    init

---
+++ AppDelegate.cs
@@ -0,0 +1,14 @@
+namespace dontet_macos_sample;
+
+[Register ("AppDelegate")]
+public class AppDelegate : NSApplicationDelegate {
+	public override void DidFinishLaunching (NSNotification notification)
+	{
+		// Insert code here to initialize your application
+	}
+
+	public override void WillTerminate (NSNotification notification)
+	{
+		// Insert code here to tear down your application
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,5 @@
+using dontet_macos_sample;
+
+// This is the main entry point of the application.
+NSApplication.Init ();
+NSApplication.Main (args);
